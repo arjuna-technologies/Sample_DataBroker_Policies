42d0baad7c8599fa0fea8c5ed0ea1faadb1190bc
Stuart Wheater <stuart.wheater@arjuna.com>
2015-03-02 10:22:36 +0000

    Added logging

--- SampleDeployPolicy/src/main/java/com/arjuna/dbpolicy/sampledeploypolicy/AcceptAllPolicy.java
+++ SampleDeployPolicy/src/main/java/com/arjuna/dbpolicy/sampledeploypolicy/AcceptAllPolicy.java
@@ -4,57 +4,69 @@
 
 package com.arjuna.dbpolicy.sampledeploypolicy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Singleton;
 import com.arjuna.agility.ServiceAgreement;
 import com.arjuna.agility.ServiceAgreementContext;
 import com.arjuna.agility.ServiceAgreementListener;
 import com.arjuna.agility.ServiceAgreementListenerException;
 import com.arjuna.agility.Vote;
-import com.arjuna.agility.view.Relationship;
-import com.arjuna.dbpolicy.sampledeploypolicy.view.SampleDeployView;
 
 @Singleton
 public class AcceptAllPolicy implements ServiceAgreementListener
 {
+    private static final Logger logger = Logger.getLogger(AcceptAllPolicy.class.getName());
+
     public void onRegistered(String domain)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onRegistered");
     }
 
     public Vote onChangeProposed(ServiceAgreement serviceAgreement, ServiceAgreementContext serviceAgreementContext)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onChangeProposed");
+
         return Vote.accept();
     }
 
     public void onChanged(ServiceAgreement serviceAgreement, ServiceAgreementContext serviceAgreementContext)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onChanged");
     }
 
     public void onChangeRejected(ServiceAgreement serviceAgreement, ServiceAgreementContext serviceAgreementContext)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onChangeRejected");
     }
 
     public Vote onTerminateProposed(ServiceAgreement serviceAgreement, ServiceAgreementContext serviceAgreementContext)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onTerminateProposed");
+
         return Vote.accept();
     }
 
     public void onTerminated(ServiceAgreement serviceAgreement, ServiceAgreementContext serviceAgreementContext)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onTerminated");
     }
 
     public void onTerminateRejected(ServiceAgreement serviceAgreement, ServiceAgreementContext serviceAgreementContext)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onTerminateRejected");
     }
 
     public void onUnregistered(String domain)
         throws ServiceAgreementListenerException
     {
+        logger.log(Level.FINE, "AcceptAllPolicy.onUnregistered");
     }
 }
